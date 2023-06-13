b92f9f5950dad3810599a0790f2a9c182c66695e
litongxy <2589994531@qq.com>
2023-06-13 13:55:00 +0800

    提交代码

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+const str = "ddddd";
+</script>
+
+<template>
+  <div class="box">
+    我是app
+    <h1>{{ str }}</h1>
+  </div>
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
